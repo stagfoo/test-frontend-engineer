5a1b4d95d4cd80f2f6831e0e484e2f2df8f56b51
alex <yo@stagfoo.com>
2025-01-12 21:53:51 +1000

    🍱 refactor: add 8 products at the start

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 import { ProductList } from '@/app/components/Productlist'
 
 
-export default async function Products() {
-  const products = await getProducts(5)
+export default async function Page() {
+  const products = await getProducts(8)
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
