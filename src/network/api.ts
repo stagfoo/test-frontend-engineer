502f8c9489cd7c5325b8eef09f52f9e321f56d29
alex <yo@stagfoo.com>
2025-01-12 20:41:22 +1000

    ✨ feature: get products list from network

--- src/network/api.ts
+++ src/network/api.ts
@@ -1,27 +1,52 @@
-type GetProducts = {
-    
+export type Product = {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    image: string;
+    rating: {
+        rate: number;
+        count: number;
+    }
+}
+
+export type GetProducts = {
+    result: Product[];
+    error: unknown
 }
 
 export async function getProducts(limit: number): Promise<GetProducts> {
     try {
-    const result = await fetch(`https://fakestoreapi.com/products?limit=${limit}`)
-    return result.json()
-    } catch {
-        throw new Error('Sorry! there is an issue accessing products at this time')
+        const result = await fetch(`https://fakestoreapi.com/products?limit=${limit}`)
+        return {
+            result: await result.json(),
+            error: null
+        }
+    } catch (err) {
+        return {
+            result: [],
+            error: err
+        }
     }
 }
-
-type GetSingleProduct = {
-    
+export type GetSingleProducts = {
+    result: Product | null;
+    error: unknown
 }
 
-export async function getSingleProduct(id: string): Promise<GetSingleProduct> {
+export async function getSingleProduct(id: string): Promise<GetSingleProducts> {
     try {
         const result = await fetch(`https://fakestoreapi.com/products/${id}`)
-        return result.json()
-        } catch {
-            throw new Error('Sorry! there is an issue getting that product at this time')
+        return {
+            result: await result.json(),
+            error: null
         }
+    } catch (err) {
+        return {
+            result: null,
+            error: err
+        }
+    }
 }
 type GetUserCart = {
 
@@ -31,9 +56,9 @@ export async function getUserCart(id: string): Promise<GetUserCart> {
     try {
         const result = await fetch(`https://fakestoreapi.com/carts/user/${id}`)
         return result.json()
-        } catch {
-            throw new Error('Sorry! there is an issue getting that product at this time')
-        }
+    } catch {
+        throw new Error('Sorry! there is an issue getting that product at this time')
+    }
 }
 
 type GetUserResponse = {
@@ -44,7 +69,7 @@ export async function getUser(id: string): Promise<GetUserResponse> {
     try {
         const result = await fetch(`https://fakestoreapi.com/users/${id}`)
         return result.json()
-        } catch {
-            throw new Error('Sorry! there is an issue getting that product at this time')
-        }
+    } catch {
+        throw new Error('Sorry! there is an issue getting that product at this time')
+    }
 }
