22ed154b6a7e0d81bd8bea0a985c514631869732
alex <yo@stagfoo.com>
2025-01-15 21:34:42 +1000

    🍱 refactor: tidy up navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { ReactNode, useState } from "react"
-import { ShoppingBagIcon, UserCircleIcon, Bars3Icon } from '@heroicons/react/24/outline'
+import { ShoppingBagIcon, UserCircleIcon, Bars3Icon, SparklesIcon } from '@heroicons/react/24/outline'
 
 export function NavItem({ href, children }: { href: string, children: ReactNode}) {
     return <a href={href}
@@ -19,7 +19,7 @@ export function Navbar(){
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="/products" className="flex items-center space-x-3 rtl:space-x-reverse ">
-        21 Grams
+        Example Shop
     </a>
     <button onClick={() => {setIsOpen(!isOpen)}} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
@@ -30,12 +30,12 @@ export function Navbar(){
       <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <li>
         <NavItem href="/products">
-        <span className="text-white">Shop</span>
+            <span className="flex flex-1 flex-row gap-2 justify-center items-center">
+            <SparklesIcon className="size-6 hidden sm:block text-white" />
+            <span className="text-white">Browse Products</span>
+            </span>
         </NavItem>
         </li>
-        <NavItem href="/cart">
-            <span className="text-white">Category</span>
-        </NavItem>
         <li>
         <NavItem href="/cart">
             <ShoppingBagIcon className="size-6 hidden sm:block text-white" />
