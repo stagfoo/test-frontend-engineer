b6d6b120871986154aa17a79278f95aa94bee8f2
alex <yo@stagfoo.com>
2025-01-15 21:34:14 +1000

    ✨ feature: add error to product list

--- src/app/components/Productlist.tsx
+++ src/app/components/Productlist.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/network/api";
 import { ProductCard } from '@/app/components/ProductCard'
+import { ErrorBlock } from "@/app/components/Error";
 
 type ProductListProps = {
     list: Product[],
@@ -7,10 +8,18 @@ type ProductListProps = {
 }
 export function ProductList({ list, error }: ProductListProps) {
     if (error) {
-        return <div>Error</div>
+        return (
+            <ErrorBlock
+                title="Oops!"
+                subtitle="Looks like there was an issue getting products at this time!"
+                byline="Please try refreshing the page with the button below"
+                linkText="Refresh Product and try again"
+                href="/products"
+            />
+        )
     }
     return (
-        <div className="grid lg:grid-cols-4 gap-4 md:grid-cols-2">
+        <div className="grid lg:grid-cols-4 gap-4 md:grid-cols-2 grid-cols-1">
             {list.map(product => {
                 return <ProductCard key={product.id} href={`/products/${product.id}`} product={product} />
             })}
